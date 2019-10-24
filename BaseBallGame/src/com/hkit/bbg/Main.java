d3336928413e39b5aba7ba0c44f2585f815bd31c
USER1 <USER1@USER7-PC>
2019-10-24 12:39:44 +0900

    입력값

--- BaseBallGame/src/com/hkit/bbg/Main.java
+++ BaseBallGame/src/com/hkit/bbg/Main.java
@@ -1,15 +1,17 @@
 package com.hkit.bbg;
 
+import java.util.Scanner;
+
 public class Main {
 	public static void main(String[] args) {
 		final int NO_COUNT = 3;
 
 		int[] randomNoArray = new int[NO_COUNT];
-		int[] userNoArray = new int[NO_COUNT];
 
 		// random 값이 0 ~ 0.999999999
 		// 따라서 randomValue값은 0 ~ 9까지의 정수
 
+		// 랜덤값 중복 x
 		for (int i = 0; i < randomNoArray.length; i++) {
 			int randomValue = (int) (Math.random() * 9) + 1;
 
@@ -17,15 +19,36 @@ public class Main {
 				if (randomNoArray[j] == randomValue) {
 					i--;
 					break;
-				}
-				else if (randomNoArray[j] == 0) {
+				} else if (randomNoArray[j] == 0) {
 					randomNoArray[j] = randomValue;
 					break;
 				}
 			}
 		}
-		for (int i = 0; i < randomNoArray.length; i++) {
-			System.out.println("randomNoArray : " + randomNoArray[i]);
+
+		// 콘솔에서 값을 입력받기 위한 객체
+		int[] userNoArray = new int[NO_COUNT];
+
+		Scanner sc = new Scanner(System.in);
+		// import 생성 단축키 Ctrl + Shift + 'o'
+
+		/*
+		 * int value1 = sc.nextInt(); System.out.println("value1 : " + value1);
+		 * 
+		 * int value2 = sc.nextInt(); System.out.println("value2 : " + value2);
+		 */
+
+		for (int i = 0; i < userNoArray.length; i++) {
+			System.out.print((i + 1) + "번째 값을 입력해 주세요 : ");
+			userNoArray[i] = sc.nextInt();
 		}
+		sc.close();
+
+		for (int item : userNoArray) {
+			System.out.println("userNoArray result : " + item);
+		}
+		System.out.print("The End");
+		
+
 	}
 }
